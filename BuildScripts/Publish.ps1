2af001d43dae89a412a74c462c265819dc01ad60
pierott <ydzialiamba@gmail.com>
2024-01-27 19:07:50 -0500

    Uncommented git tag

--- BuildScripts/Publish.ps1
+++ BuildScripts/Publish.ps1
@@ -6,4 +6,4 @@ $tagName = "v$version"
 dotnet tool update -g vpk
 dotnet publish -c Release --self-contained -r win-x64 -o $rootPath\WebpWhack\bin\publish
 vpk pack -u WebpWhack -v $version -p $rootPath\WebpWhack\bin\publish -o $rootPath\WebpWhack\bin\Releases -e WebpWhack.exe
-#git tag $tagName
+git tag $tagName
