1abd53d1ac1ccaac8d150e8dc692d4d565389cad
pierott <ydzialiamba@gmail.com>
2024-01-28 19:12:12 -0500

    Added debug version

--- BuildScripts/CheckVersion.ps1
+++ BuildScripts/CheckVersion.ps1
@@ -10,7 +10,19 @@
 . $PSScriptRoot\Common.ps1
 
 $oldCd = pwd
+$oldEcd = [Environment]::CurrentDirectory
+
+Write-Host $oldCd
+Write-Host $([Environment]::CurrentDirectory)
+Write-Host $([Environment]::CommandLine)
+
 cd $rootPath
+[Environment]::CurrentDirectory = $rootPath
+
+Write-Host -----------
+
+Write-Host $(pwd)
+Write-Host $([Environment]::CurrentDirectory)
 
 $version = Get-Content -Path "$rootPath\version.txt" -Raw
 $tagName = "v$version"
@@ -20,14 +32,17 @@ try {
     if($LASTEXITCODE -eq 0) {
         Write-Host "Tag $tagName already exists"
         cd $oldCd
+        [Environment]::CurrentDirectory = $oldEcd
         exit 1
     } else {
         Write-Host "Tag $tagName does not exist"
         cd $oldCd
+        [Environment]::CurrentDirectory = $oldEcd
         exit 0
     }
 } catch {
     Write-Error $_
     cd $oldCd
+    [Environment]::CurrentDirectory = $oldEcd
     exit 1
 }
--- BuildScripts/Publish.ps1
+++ BuildScripts/Publish.ps1
@@ -4,7 +4,11 @@ $version = Get-Content -Path "$rootPath\version.txt" -Raw
 $tagName = "v$version"
 
 $oldCd = pwd
+
 cd $rootPath
+
+Write-Host [Environment]::CurrentDirectory
+Write-Host pwd
 
 dotnet tool update -g vpk
 dotnet publish -c Release --self-contained -r win-x64 -o $rootPath\WebpWhack\bin\publish
