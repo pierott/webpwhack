eb261ff5149879b01e3553cd4778dd0b81669428
pierott <ydzialiamba@gmail.com>
2024-01-28 19:32:03 -0500

    This time it should work!

--- BuildScripts/CheckVersion.ps1
+++ BuildScripts/CheckVersion.ps1
@@ -10,32 +10,25 @@
 . $PSScriptRoot\Common.ps1
 
 $oldCd = pwd
-$oldEcd = [Environment]::CurrentDirectory
-
 cd $rootPath
-[Environment]::CurrentDirectory = $rootPath
 
 $version = Get-Content -Path "$rootPath\version.txt" -Raw
 $tagName = "v$version"
 
 try {
-    git status
-    git tag
-    git rev-parse --verify --quiet "refs/tags/$tagName"
-    if($LASTEXITCODE -eq 0) {
+    $tagExists = git ls-remote origin --tags $tagName
+    
+    if($tagExists) {
         Write-Host "Tag $tagName already exists"
         cd $oldCd
-        [Environment]::CurrentDirectory = $oldEcd
         exit 1
     } else {
-        Write-Host "Tag $tagName does not exist"
+        Write-Host "Tag $tagName doesn't exist"
         cd $oldCd
-        [Environment]::CurrentDirectory = $oldEcd
         exit 0
     }
 } catch {
     Write-Error $_
     cd $oldCd
-    [Environment]::CurrentDirectory = $oldEcd
     exit 1
 }
